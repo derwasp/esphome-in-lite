75bc1e06b4f48742b37021f32c15274b0222e44c
Denys Zhuravel <nbcwasp@gmail.com>
2026-03-21 21:16:18 +0000

    Decouple state refresh scheduling from poll interval

--- components/inlite_hub/inlite_hub.cpp
+++ components/inlite_hub/inlite_hub.cpp
@@ -228,13 +228,12 @@ void InliteHub::loop() {
     return;
   }
 
+  // Enforce refresh cadence independently of PollingComponent update() cadence.
+  this->queue_state_sync_request_(false);
   this->process_active_stream_();
 }
 
-void InliteHub::update() {
-  this->request_rssi_();
-  this->queue_state_sync_request_(false);
-}
+void InliteHub::update() { this->request_rssi_(); }
 
 void InliteHub::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if,
                                     esp_ble_gattc_cb_param_t *param) {
